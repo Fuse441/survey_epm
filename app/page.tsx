84103608a6c74c782a7f676701dcd004b97d2137
Fuse441 <jiranon.khemklad@gmail.com>
2025-03-06 23:10:02 +0700

    update config and fix template

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { Input, Textarea } from "@heroui/input";
 import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
 import { Button } from "@heroui/button";
 import React from "react";
-
+import { Form } from "@heroui/form";
 import {
   groupTypeBusiness,
   registrationNumber,
@@ -19,21 +19,71 @@ import {
   typeOfBusiness,
 } from "@/config/configForm";
 import Questions from "@/components/question";
+
 export default function Home() {
+  const [selected, setSelected] = useState<{ [key: string]: string | string[] }>({});
   const [isAuthen, setIsAuthen] = useState<string>();
+  const [regisNumber, setRegisNumber] = useState<string>();
+  const [securityCode, setSecurityCode] = useState<string>();
+  const [establishmentName, setEstablishmentName] = useState<string>();
   const [provinces, setProvinces] = useState<string>();
   const [amphures, setAmphures] = useState<any>();
   const [tambons, setTambons] = useState<any>();
-  const [zipCode,setZipCode] = useState<string>("");
-  const [provideSkills,setProvideskills] = useState<string>();
-  const [typeBusiness,setTypeBusiness] = useState<string>();
+  const [zipCode, setZipCode] = useState<string>("");
+  const [provideSkills, setProvideskills] = useState<string>();
+  const [typeBusiness, setTypeBusiness] = useState<string>();
+  const [branch, setBranch] = useState<string>();
+  const [size, setSize] = useState<string>();
   const router = useRouter();
+  const [stateError, setStateError] = useState({
+    vaildate_EstablishmentName: {
+      status: false,
+    },
+    vaildate_Branch : {
+      status : false
+    },
+    vaildate_TypeBusiness : {
+      status : false
+    },
+    vaildate_Size : {
+      status : false
+    }
+  });
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
+  const updateStatus = (key: string | number, value: any) => {
+    setStateError((prevState: any) => ({
+      ...prevState,
+      [key]: { ...prevState[key], status: value },
+    }));
+  };
+  const handleSubmit = () => {
+    const validationErrors = {
+        vaildate_EstablishmentName: !establishmentName,
+        vaildate_Branch: !branch,
+        vaildate_TypeBusiness: !typeBusiness,
+        vaildate_Size : !size
+    };
 
-  //   token ? "" : router.push("/login");
-  // }, []);
+    Object.entries(validationErrors).forEach(([key, value]) => {
+        if (value) updateStatus(key, true);
+    });
+
+    console.log({
+        regisNumber,
+        securityCode,
+        establishmentName,
+        branch,
+        typeBusiness,
+        size,
+        provideSkills,
+        provinces,
+        amphures,
+        tambons,
+        zipCode,
+        selected
+    });
+};
+
   const onProvinceChange = (value: any) => {
     setProvinces(value);
 
@@ -44,35 +94,32 @@ export default function Home() {
     }
 
     const selectObject = thaiAmphures.filter(
-      (item) => item.province_id === value.id,
+      (item) => item.province_id === value.id
     );
 
     setAmphures(selectObject);
-
-    
   };
   const onAmphureChange = (value: any) => {
-      if(!value){
-        setTambons([]);
-      }
-    const selectObject = (value && thaiTambons.filter((item) => item.amphure_id == value.id))
+    if (!value) {
+      setTambons([]);
+    }
+    const selectObject =
+      value && thaiTambons.filter((item) => item.amphure_id == value.id);
 
-    setTambons(selectObject)
-  }
-  const onTambonChange = (value:any) => {
-  
-    if(!value){
-      setZipCode("")
+    setTambons(selectObject);
+  };
+  const onTambonChange = (value: any) => {
+    if (!value) {
+      setZipCode("");
 
       return;
     }
-    value &&  setZipCode(value.zip_code)
-   console.log(value)
-   
-  }
+    value && setZipCode(value.zip_code);
+  };
 
   return (
     <>
+     {/* <pre>{JSON.stringify(selected, null, 2)}</pre> */}
       <div className="mb-10 flex justify-center">
         <h1 className="text-3xl">
           แบบสำรวจความต้องการพัฒนาฝีมือแรงงานของสถานประกอบการ
@@ -84,42 +131,102 @@ export default function Home() {
             ข้อมูลสถานประกอบการ
           </CardHeader>
           <Divider />
-          <CardBody >
+          <CardBody>
             <p className="text-red-500">
               *กรอกข้อมูลของท่านด้วยเลขทะเบียนพาณิชย์
               หรือเลขประกันสังคมอย่างใดอย่างหนึ่ง
             </p>
             <div className="mt-5 grid grid-cols-3 gap-4 w-full">
-              <Input className="col-span-1" label="เลขทะเบียนพาณิชย์" />
-              <Input className="col-span-1" label="สาขา">
-                {registrationNumber.map((number, index) => (
-                  <AutocompleteItem key={index}>{number}</AutocompleteItem>
-                ))}
-              </Input>
-              <Input className="col-span-1" label="รหัสประกันสังคม" />
-              <Input className="col-span-3" label="ชื่อสถานประกอบการ" />
+              <div className="col-span-3 grid grid-cols-6 gap-4">
+                <Input
+                  className="col-span-3"
+                  isDisabled={securityCode && !regisNumber ? true : false}
+                  label="เลขทะเบียนพาณิชย์"
+                  placeholder="กรอกเลขทะเบียนพาณิชย์"
+                  value={regisNumber}
+                  onValueChange={setRegisNumber}
+                />
+                <Input
+                  className="col-span-3"
+                  isDisabled={!securityCode && regisNumber ? true : false}
+                  label="รหัสประกันสังคม"
+                  placeholder="กรอกรหัสประกันสังคม"
+                  value={securityCode}
+                  onValueChange={setSecurityCode}
+                />
+              </div>
+
+              <Input
+                isRequired
+                errorMessage={"กรุณากรอกชื่อสถานประกอบการ"}
+                isInvalid={
+                  !establishmentName &&
+                  stateError.vaildate_EstablishmentName.status
+                }
+                className="col-span-2"
+                label="ชื่อสถานประกอบการ"
+                placeholder="กรอกชื่อสถานประกอบการ"
+                value={establishmentName}
+                onValueChange={setEstablishmentName}
+              />
+
+              <Input
+                isRequired
+                errorMessage={"กรุณากรอกสาขา"}
+                isInvalid={
+                  !branch &&
+                  stateError.vaildate_Branch.status
+                }
+                className="col-span-1"
+                label="สาขา"
+                placeholder="กรอกสาขา"
+                value={branch}
+                onValueChange={setBranch}
+              />
+
               <div className="grid grid-cols-6 gap-4 col-span-3">
-              <Autocomplete className="col-span-3" label="ประเภทกิจการ" placeholder="ใส่เลข TSIC" onInputChange={(event)=> {
-                const selectObject = typeOfBusiness.find((item) => item.label == event)
-                setTypeBusiness(selectObject?.TSIC_ID)
-                console.log(typeBusiness)
-              }}>
-                {typeOfBusiness.map((item, index) => (
-                  <AutocompleteItem key={index}>{item.label}</AutocompleteItem>
-                ))}
-              </Autocomplete>
-              <Input className=" col-span-3" label="กลุ่มประเภทกิจการ"  isDisabled={true} value={groupTypeBusiness.find((item) => item.value === typeBusiness)?.label ?? ""}>
-              </Input>
+                <Autocomplete
+                   isRequired
+                  isInvalid={!typeBusiness && stateError.vaildate_TypeBusiness.status}
+                  errorMessage="กรุณาเลือกประเภทกิจการ"
+                  className="col-span-3"
+                  label="ประเภทกิจการ"
+                  placeholder="เลือก TSIC"
+                  onInputChange={(event) => {
+                    const selectObject = typeOfBusiness.find(
+                      (item) => item.label == event
+                    );
+
+                    setTypeBusiness(selectObject?.TSIC_ID);
+                    console.log(typeBusiness);
+                  }}
+                >
+                  {typeOfBusiness.map((item, index) => (
+                    <AutocompleteItem key={index}>
+                      {item.label}
+                    </AutocompleteItem>
+                  ))}
+                </Autocomplete>
+                <Input
+                  className=" col-span-3"
+                  isDisabled={true}
+                  label="กลุ่มประเภทกิจการ"
+                  value={
+                    groupTypeBusiness.find(
+                      (item) => item.value === typeBusiness
+                    )?.label ?? ""
+                  }
+                />
               </div>
               <div className="col-span-3">
-                <RadioGroup className="w-full" label="ขนาดสถานประกอบการ">
+                <RadioGroup className="w-full" label="ขนาดสถานประกอบการ" isRequired errorMessage="กรุณาเลือกขนาดสถานประกอบการ" isInvalid={!size && stateError.vaildate_Size.status} value={size} onValueChange={setSize} >
                   <div className="flex flex-row gap-4">
-                    <Radio value="buenos-aires">น้อยกว่า 10 คน</Radio>
-                    <Radio value="sydney">10-19 คน</Radio>
-                    <Radio value="san-francisco">20-49 คน</Radio>
-                    <Radio value="london">50-99 คน</Radio>
-                    <Radio value="tokyo">100-199 คน</Radio>
-                    <Radio value="tokyo1">200 คนขึ้นไป</Radio>
+                    <Radio value="less-10">น้อยกว่า 10 คน</Radio>
+                    <Radio value="10-19">10-19 คน</Radio>
+                    <Radio value="20-49">20-49 คน</Radio>
+                    <Radio value="50-99">50-99 คน</Radio>
+                    <Radio value="100-199">100-199 คน</Radio>
+                    <Radio value="more-200">200 คนขึ้นไป</Radio>
                   </div>
                 </RadioGroup>
               </div>
@@ -136,15 +243,16 @@ export default function Home() {
                 isVirtualized
                 className="col-span-1"
                 label="จังหวัด"
+                placeholder="เลือกจังหวัด"
                 onInputChange={(value) => {
-                  const selectObject = thaiProvinces &&  thaiProvinces.find(
-                    (item) => item.name_th === value,
-                  );
+                  const selectObject =
+                    thaiProvinces &&
+                    thaiProvinces.find((item) => item.name_th === value);
 
                   onProvinceChange(
                     selectObject
                       ? { id: selectObject.id, name: selectObject.name_th }
-                      : null,
+                      : null
                   );
                 }}
               >
@@ -155,18 +263,21 @@ export default function Home() {
                 ))}
               </Autocomplete>
               <Autocomplete
-              isVirtualized
+                isVirtualized
                 className="col-span-1"
                 isDisabled={provinces == null ? true : false}
                 label="อำเภอ"
+                placeholder="เลือกอำเภอ"
                 onInputChange={(value) => {
-                  const selectObject = thaiAmphures && thaiAmphures.find(
-                    (item) => item.name_th == value
-                  )
+                  const selectObject =
+                    thaiAmphures &&
+                    thaiAmphures.find((item) => item.name_th == value);
 
                   onAmphureChange(
-                    (selectObject ? {id: selectObject.id,name : selectObject.name_th} : null) 
-                  )
+                    selectObject
+                      ? { id: selectObject.id, name: selectObject.name_th }
+                      : null
+                  );
                 }}
               >
                 {amphures &&
@@ -177,65 +288,106 @@ export default function Home() {
                   ))}
               </Autocomplete>
               <Autocomplete
-              isVirtualized
+                isVirtualized
                 className="col-span-1"
-                isDisabled={
-                   (!amphures || amphures == '')  ? true : false
-                }
+                isDisabled={!amphures || amphures == "" ? true : false}
                 label="ตำบล"
+                placeholder="เลือกตำบล"
                 onInputChange={(value) => {
+                  const selectObject =
+                    tambons &&
+                    tambons.find((item: any) => item.name_th == value);
 
-                  const selectObject = tambons && tambons.find((item:any) => item.name_th == value)
-
-                  onTambonChange(selectObject)
-                  
-                }
-
-                }
+                  onTambonChange(selectObject);
+                }}
               >
-                {tambons && tambons!.map((item:any, index:any) => (
-                  <AutocompleteItem key={index}>{item.name_th}</AutocompleteItem>
-                ))}
+                {tambons &&
+                  tambons!.map((item: any, index: any) => (
+                    <AutocompleteItem key={index}>
+                      {item.name_th}
+                    </AutocompleteItem>
+                  ))}
               </Autocomplete>
-              <Input className="col-span-1" label="รหัสไปรษณีย์" value={zipCode} />
-              <Input className="col-span-2" label="เบอร์โทรศัพท์" type="number" />
+              <Input
+                className="col-span-1"
+                label="รหัสไปรษณีย์"
+                placeholder="กรอกรหัสไปรษณีย์"
+                value={zipCode}
+              />
+              <Input
+                className="col-span-2"
+                label="เบอร์โทรศัพท์"
+                placeholder="กรอกเบอร์โทรศัพท์"
+                type="number"
+              />
 
               <h1 className="mx-2 text-xl col-span-3 text-stone-950">
-          รายละเอียดผู้ตอบแบบสำรวจ
-
-        </h1>
-        <Input className="col-span-1" label="ชื่อ" />
-        <Input className="col-span-1" label="นามสกุล" />
-        <Input className="col-span-1" label="เบอร์โทรศัพท์" type="number" />
-        <Input className="col-span-3" label="อีเมล" type="email" />
-        <Input className="col-span-3" label="ตำแหน่ง"  />
+                รายละเอียดผู้ตอบแบบสำรวจ
+              </h1>
+              <Input
+                className="col-span-1"
+                label="ชื่อ"
+                placeholder="กรอกชื่อ"
+              />
+              <Input
+                className="col-span-1"
+                label="นามสกุล"
+                placeholder="กรอกนามสกุล"
+              />
+              <Input
+                className="col-span-1"
+                label="เบอร์โทรศัพท์"
+                placeholder="กรอกเบอร์โทรศัพท์"
+                type="number"
+              />
+              <Input
+                className="col-span-3"
+                label="อีเมล"
+                placeholder="กรอกอีเมล"
+                type="email"
+              />
+              <Input
+                className="col-span-3"
+                label="ตำแหน่ง"
+                placeholder="กรอกตำแหน่ง"
+              />
 
               <div className="questions col-span-3 mt-3">
                 <h1 className="text-xl">คำถาม</h1>
-                    <Questions></Questions>
+                <Questions selected={selected} setSelected={setSelected} />
               </div>
-        
-        <h1 className="mx-2 mt-3 text-xl col-span-3 text-stone-950">
-          ข้อมูลความต้องการพัฒนาทักษะแรงงานหรือไม่?
 
-        </h1>
-        {/* <h1 className="mx-2 mt-3 text-xl col-span-3 text-stone-950">
+              <h1 className="mx-2 mt-3 text-xl col-span-3 text-stone-950">
+                ข้อมูลความต้องการพัฒนาทักษะแรงงานหรือไม่?
+              </h1>
+              {/* <h1 className="mx-2 mt-3 text-xl col-span-3 text-stone-950">
           ปัจจุบันบริษัทขงท่านมีการอบรมหรือพัฒนาทักษะให้กับพนักงาน หรือไม่?
         </h1> */}
-      
-        <RadioGroup className="col-span-3" defaultValue="has"  label="ปัจจุบันบริษัทขงท่านมีการอบรมหรือพัฒนาทักษะให้กับพนักงาน" onValueChange={(value) => setProvideskills(value)}> 
-          <div className="flex flex-col gap-3">
-      
-            <Radio  className="w-[350px]" value="has" >มี </Radio> <Input isDisabled={provideSkills == "dontHas"} label="(ระบุประเภทของการอบรม)" />
 
-            
-      <Radio value="dontHas" >ไม่มี</Radio>
-</div>
-    
-    </RadioGroup>
+              <RadioGroup
+                className="col-span-3"
+                defaultValue="has"
+                label="ปัจจุบันบริษัทขงท่านมีการอบรมหรือพัฒนาทักษะให้กับพนักงาน"
+                onValueChange={(value) => setProvideskills(value)}
+              >
+                <div className="flex flex-col gap-3">
+                  <Radio className="w-[350px]" value="has">
+                    มี{" "}
+                  </Radio>{" "}
+                  <Input
+                    isDisabled={provideSkills == "dontHas"}
+                    label="(ระบุประเภทของการอบรม)"
+                  />
+                  <Radio value="dontHas">ไม่มี</Radio>
+                </div>
+              </RadioGroup>
             </div>
 
-            <Button className="text-sky-50 mt-5" color="success">
+            <Button
+              className="text-sky-50 mt-5"
+              color="success"
+              onPress={handleSubmit}
+            >
               ยืนยัน
             </Button>
           </CardBody>
--- components/question.tsx
+++ components/question.tsx
@@ -1,70 +1,116 @@
 "use client";
 
 import { useState } from "react";
-import { RadioGroup, Radio } from "@heroui/radio";
+import { Radio, RadioGroup } from "@heroui/radio";
+import { Checkbox, CheckboxGroup } from "@heroui/checkbox";
+import { Divider } from "@heroui/divider";
+import { Accordion, AccordionItem } from "@heroui/accordion";
+
 import { questions } from "@/config/questions";
-import { CheckboxGroup, Checkbox } from "@heroui/checkbox";
-
-export default function Questions() {
-  const [selected, setSelected] = useState<{ [key: string]: string | string[] }>({});
-
+type QuestionsProps = {
+  selected: { [key: string]: string | string[] };
+  setSelected: React.Dispatch<React.SetStateAction<{ [key: string]: string | string[] }>>;
+};
+export default function Questions({ selected, setSelected }: QuestionsProps) {
+  
   return (
     <div>
+
+
       {questions.map((item, index) => (
-        <div className="questions" key={index}>
-          <h2 className="text-lg">{item.question}</h2>
-          {item.sub_question.map((sub, id_sub) => {
-            const key = `${index+1}.${id_sub+1}`; 
+        <div key={index} className="questions">
+          <Accordion>
+            <AccordionItem
+              key={index}
+              aria-label={item.question}
+              title={item.question}
+            >
+              {item.sub_question.map((sub, id_sub) => {
+                const key = `${index + 1}.${id_sub + 1}`;
 
-            return (
-              <div className="sub_question m-2" key={id_sub}>
-                <h2 className="text-lg">{`${index + 1}.${id_sub + 1} ${sub.question}`}</h2>
-                <div className="choices mt-2">
-                  {item.type === "radio" && (
-                    <RadioGroup
-                      value={typeof selected[key] === "string" ? selected[key] : ""}
-                      onValueChange={(newValue) =>
-                        setSelected((prev) => ({
-                          ...prev,
-                          [key]: newValue, // ใช้ key ที่กำหนด
-                        }))
-                      }
-                      className=""
-                    >
-                      <div className="flex flex-col gap-3">
-                        {sub.choices.map((option, index) => (
-                          <Radio key={`${sub.id}-${index}`} value={option}>
-                            {option}
-                          </Radio>
-                        ))}
-                      </div>
-                    </RadioGroup>
-                  )}
+                return (
+                  <div key={id_sub} className="sub_question m-2 mb-3">
+                    <h2 className="text-lg">{sub.label}</h2>
+                    {/* <h2 className="text-lg">{`${index + 1}.${id_sub + 1} ${sub.question}`}</h2> */}
+                    <h2 className="text-lg ml-4">{`${sub.question}`}</h2>
+                    <div className="choices mt-2 ml-5">
+                      {sub.type === "radio" && (
+                        <RadioGroup
+                          className=""
+                          value={
+                            typeof selected[key] === "string"
+                              ? selected[key]
+                              : ""
+                          }
+                          onValueChange={(newValue) =>
+                            setSelected((prev) => ({
+                              ...prev,
+                              [key]: newValue, // ใช้ key ที่กำหนด
+                            }))
+                          }
+                        >
+                          <div className="flex flex-col gap-3">
+                            {sub.choices.map((option, index) => (
+                              <Radio key={`${sub.id}-${index}`} value={option}>
+                                {option}
+                              </Radio>
+                            ))}
+                          </div>
+                        </RadioGroup>
+                      )}
 
-                  {item.type === "checkbox" && (
-                    <CheckboxGroup
-                      value={Array.isArray(selected[key]) ? selected[key] : []}
-                      onChange={(newValues) =>
-                        setSelected((prev) => ({
-                          ...prev,
-                          [key]: newValues, // เก็บค่าเป็น array
-                        }))
-                      }
-                      className=""
-                    >
-                      <div className="flex flex-col gap-3">
-                        {sub.choices.map((option, index) => (
-                          <Checkbox key={`${sub.id}-${index}`} value={option}>
-                            {option}
-                          </Checkbox>
-                        ))}
-                      </div>
-                    </CheckboxGroup>
-                  )}
-                </div>
-              </div>
-            );
-          })}
+                      {sub.type === "checkbox" && (
+                        <CheckboxGroup
+                          className=""
+                          value={
+                            Array.isArray(selected[key]) ? selected[key] : []
+                          }
+                          onChange={(newValues) => {
+                            const selectedValue = newValues.find((item) => (item == "ไม่ทราบ" || item == "ไม่ทราบทักษะที่ควรพัฒนา" || item == "ไม่ต้องการพัฒนาทักษะสำหรับฝ่ายนี้"))
+
+                            console.log(selectedValue);
+                            if (selectedValue) {
+                              setSelected((prev) => ({
+                                ...prev,
+                                [key]: [selectedValue],
+                              }));
+                            } else {
+                              setSelected((prev) => ({
+                                ...prev,
+                                [key]: newValues, // ปกติให้เก็บค่าตามที่เลือก
+                              }));
+                            }
+                          }}
+                        >
+                          <div className="flex flex-col gap-3">
+                            {sub.choices.map((option, index) => (
+                              <Checkbox
+                                key={`${sub.id}-${index}`}
+                                isDisabled={
+                                  (selected[key]?.includes("ไม่ทราบ") || 
+                                   selected[key]?.includes("ไม่ทราบทักษะที่ควรพัฒนา") ||  
+                                   selected[key]?.includes("ไม่ต้องการพัฒนาทักษะสำหรับฝ่ายนี้")) &&
+                                  (option !== "ไม่ทราบ" && 
+                                   option !== "ไม่ทราบทักษะที่ควรพัฒนา" && 
+                                   option !== "ไม่ต้องการพัฒนาทักษะสำหรับฝ่ายนี้") ||
+                                  (selected[key]?.includes("ไม่ทราบทักษะที่ควรพัฒนา") && option === "ไม่ต้องการพัฒนาทักษะสำหรับฝ่ายนี้") ||
+                                  (selected[key]?.includes("ไม่ต้องการพัฒนาทักษะสำหรับฝ่ายนี้") && option === "ไม่ทราบทักษะที่ควรพัฒนา")
+                                }
+                                value={option}
+                              >
+                                {option}
+                              </Checkbox>
+                            ))}
+                          </div>
+                        </CheckboxGroup>
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
+            </AccordionItem>
+          </Accordion>
+          <Divider />
         </div>
       ))}
 
